e18d0f771b68e58ef549fed005953fddd79ca856
Blezzing <mmhmaster@hotmail.com>
2014-11-07 19:23:19 +0100

    Spawner v1
    Spawner har mulighed for at lave player, men bruger ikkedefineret sprite
    atm.

--- Code/WiT/WiTProject/Core/Spawner.cs
+++ Code/WiT/WiTProject/Core/Spawner.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿#region Using Statements
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WaveEngine.Common;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework.Physics2D;
+using WaveEngine.Components.Cameras;
+using WaveEngine.Components.Graphics2D;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Common.Input;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Resources;
+using WaveEngine.Framework.Services;
+using WaveEngine.Framework.Managers;
+#endregion
+
 
 namespace WiTProject
 {
-    class Spawner
+    public class Spawner
     {
+        //dictionary over sprites
+        private Dictionary<string, string> _spriteDictionary = new Dictionary<string, string>();
+
+        //vars
+        private Scene _scene;
+
+        //constructor
+        public Spawner(Scene scene)
+        {
+            _scene = scene;
+            _spriteDictionary.Add("PlayerSprite", "Content/Sprites/blablabla"); //FIX
+        }
+
+        public void Player(float posX, float posY)
+        {
+            Entity player = new Entity("Player")
+                .AddComponent(new Transform2D() { Position = new Vector2(posX, posY) })
+                .AddComponent(new RigidBody2D() { IgnoreGravity = true })
+                .AddComponent(new Sprite(_spriteDictionary["PlayerSprite"]))
+                .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
+                .AddComponent(new CircleCollider())
+                .AddComponent(new PlayerBehavior());
+            _scene.EntityManager.Add(player);
+        }
     }
 }
--- Code/WiT/WiTProject/MyScene.cs
+++ Code/WiT/WiTProject/MyScene.cs
@@ -16,39 +16,21 @@ namespace WiTProject
 {
     public class MyScene : Scene
     {
+        private Spawner _spawner;
+
         protected override void CreateScene()
         {
-            //Insert your scene definition here.
-
-            #region Simple test
-            //Create a 3D camera
-            var camera3D = new FreeCamera("Camera3D", new Vector3(0, 2, 4), Vector3.Zero) { BackgroundColor = Color.CornflowerBlue };
-            EntityManager.Add(camera3D);
-
-            // Draw a cube
-            Entity cube = new Entity()
-                .AddComponent(new Transform3D())
-                .AddComponent(Model.CreateCube())
-                .AddComponent(new Spinner() { AxisTotalIncreases = new Vector3(1, 2, 3) })
-                .AddComponent(new MaterialsMap())
-                .AddComponent(new ModelRenderer());
-
-            EntityManager.Add(cube);
+            //Setup spawner
+            _spawner = new Spawner(this);
 
             // Create a 2D camera
             var camera2D = new FixedCamera2D("Camera2D") { ClearFlags = ClearFlags.DepthAndStencil }; // Transparent background need this clearFlags.
             EntityManager.Add(camera2D);
 
-            // Draw a simple sprite
-            Entity sprite = new Entity()
-                .AddComponent(new Transform2D())
-                // Change this line for a custom assets "new Sprite("Content/MyTexture"))"
-                // Manage assets using the Resources.weproj link to open the Assets Exporter tool.
-                .AddComponent(new Sprite(StaticResources.DefaultTexture))
-                .AddComponent(new SpriteRenderer(DefaultLayers.Opaque));
+            //Add player
+            _spawner.Player(50, 50);
 
-            EntityManager.Add(sprite);
-            #endregion
+
         }
 
         protected override void Start()
